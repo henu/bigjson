7aaf12a310af6fbb507b6e0e6b962dd58dbd9653
Henrik Heino <henrik.heino@gmail.com>
2016-08-03 04:34:40 +0300

    First test version that can read an array of booleans and nulls.

---
+++ bigjson.py
@@ -0,0 +1,161 @@
+class BigJSON:
+
+    _WHITESPACE = '\t\n '
+    _READBUF_CHUNK_SIZE = 1024*1024
+
+    def __init__(self, file):
+        self.file = file
+
+        # This buffer is for reading and peeking
+        self.readbuf = ''
+
+        self.root_node = Node(self)
+
+    def __getitem__(self, index):
+        return self.root_node[index]
+
+    def _skip_whitespace(self):
+        while True:
+            # Read whitespace from current readbuffer
+            whitespace_in_readbuf = 0
+            while whitespace_in_readbuf < len(self.readbuf) and self.readbuf[whitespace_in_readbuf] in BigJSON._WHITESPACE:
+                whitespace_in_readbuf += 1
+            self.readbuf = self.readbuf[whitespace_in_readbuf:]
+
+            # If readbuffer is not empty, then read more
+            if len(self.readbuf) > 0:
+                return
+
+            # Readbuffer is empty, so load more
+            self._fill_readbuf(BigJSON._READBUF_CHUNK_SIZE)
+
+    def _is_next(self, s):
+        self._fill_readbuf(len(s))
+        return self.readbuf.startswith(s)
+
+    def _skip_if_next(self, s):
+        """ If next bytes are same as in 's', then skip them and return True.
+        """
+        if self._is_next(s):
+            self.readbuf = self.readbuf[len(s):]
+            return True
+        return False
+
+    def _fill_readbuf(self, desired_size):
+        read_amount = desired_size - len(self.readbuf)
+        if read_amount <= 0:
+            return
+        self.readbuf += self.file.read(read_amount)
+
+    def _tell_read_pos(self):
+        return self.file.tell() - len(self.readbuf)
+
+    def _seek(self, pos):
+        self.readbuf = ''
+        self.file.seek(pos)
+
+
+class Node:
+
+    TYPE_NULL = 0
+    TYPE_FALSE = 1
+    TYPE_TRUE = 2
+    TYPE_NUM = 3
+    TYPE_STRING = 4
+    TYPE_ARRAY = 5
+    TYPE_OBJECT = 6
+
+    def __init__(self, bj):
+        self.bj = bj
+
+        self.bj._skip_whitespace()
+
+        # Read part of file to analyze type of it
+        if self.bj._skip_if_next('null'):
+            self.type = Node.TYPE_NULL
+        elif self.bj._skip_if_next('false'):
+            self.type = Node.TYPE_FALSE
+        elif self.bj._skip_if_next('true'):
+            self.type = Node.TYPE_TRUE
+        # TODO: Check num!
+        # TODO: Check string!
+        elif self.bj._skip_if_next('['):
+            self.type = Node.TYPE_ARRAY
+            self.fully_read = False
+            # TODO: Replace this single lookup position with some kind of lookup table!
+            self.children_begin = self.bj._tell_read_pos()
+        elif self.bj._skip_if_next('{'):
+            self.type = Node.TYPE_OBJECT
+            self.fully_read = False
+            raise Exception('Not implemented yet!')
+        else:
+            raise Exception('Unexpected bytes!')
+
+    def get_type(self):
+        return ['null', 'false', 'true', 'number', 'string', 'array', 'object'][self.type]
+
+    def __getitem__(self, index):
+        if self.type == Node.TYPE_ARRAY:
+
+            # TODO: Support negative indexes!
+
+            # Rewind to requested child
+            self.bj._seek(self.children_begin)
+            self.bj._skip_whitespace()
+            while True:
+                if self.bj._is_next(']'):
+                    raise IndexError('Out of range!')
+
+                child = Node(self.bj)
+                child._read_fully()
+
+                if index == 0:
+                    return child._get_value()
+
+                # Skip comma and whitespace around it
+                self.bj._skip_whitespace()
+                if not self.bj._skip_if_next(','):
+                    raise IndexError('Out of range!')
+                self.bj._skip_whitespace()
+
+                index -= 1
+
+        elif self.type == Node.TYPE_OBJECT:
+
+            raise Exception('NOT IMPLEMENTED YET!')
+
+        elif self.type == Node.TYPE_STRING:
+
+            return self.string[index]
+
+        else:
+
+            raise Exception('Index operator does not work with type "{}"!'.format(self.get_type))
+
+    def _get_value(self):
+        """ If possible, returns basic python object, but
+        with complex Nodes, returns the Node itself.
+        """
+        if self.type == Node.TYPE_NULL:
+            return None
+        if self.type == Node.TYPE_FALSE:
+            return False
+        if self.type == Node.TYPE_TRUE:
+            return True
+        if self.type == Node.TYPE_NUM:
+            return self.number
+        if self.type == Node.TYPE_STRING:
+            return self.string
+        return self
+
+    def _read_fully(self):
+
+        # If already fully read
+        if self.type in [Node.TYPE_NULL, Node.TYPE_FALSE, Node.TYPE_TRUE, Node.TYPE_NUM, Node.TYPE_STRING]:
+            return
+
+        if self.type == Node.TYPE_ARRAY:
+            raise Exception('Not implemented yet!')
+
+        else:
+            raise Exception('Not implemented yet!')
