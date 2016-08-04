6ee02dd0e3be575ec05ce5b8026d7f6585cf2087
Henrik Heino <henrik.heino@gmail.com>
2016-08-04 23:26:25 +0300

    Parsing of number value.

--- bigjson.py
+++ bigjson.py
@@ -29,6 +29,20 @@ class BigJSON:
             # Readbuffer is empty, so load more
             self._fill_readbuf(BigJSON._READBUF_CHUNK_SIZE)
 
+    def _get(self):
+        self._fill_readbuf(1)
+        if len(self.readbuf) == 0:
+            raise Exception('Unexpected end of file when getting next byte!')
+        result = self.readbuf[0]
+        self.readbuf = self.readbuf[1:]
+        return result
+
+    def _peek(self):
+        self._fill_readbuf(1)
+        if len(self.readbuf) == 0:
+            return None
+        return self.readbuf[0]
+
     def _is_next(self, s):
         self._fill_readbuf(len(s))
         return self.readbuf.startswith(s)
@@ -60,7 +74,7 @@ class Node:
     TYPE_NULL = 0
     TYPE_FALSE = 1
     TYPE_TRUE = 2
-    TYPE_NUM = 3
+    TYPE_NUMBER = 3
     TYPE_STRING = 4
     TYPE_ARRAY = 5
     TYPE_OBJECT = 6
@@ -73,21 +87,65 @@ class Node:
         # Read part of file to analyze type of it
         if self.bj._skip_if_next('null'):
             self.type = Node.TYPE_NULL
+
         elif self.bj._skip_if_next('false'):
             self.type = Node.TYPE_FALSE
+
         elif self.bj._skip_if_next('true'):
             self.type = Node.TYPE_TRUE
-        # TODO: Check num!
+
+        elif self.bj._peek() in '-0123456789':
+            self.type = Node.TYPE_NUMBER
+            num = self.bj._get()
+            # Check sign
+            if num == '-':
+                num += self.bj._get()
+            # Read integer part
+            if num[-1] != '0':
+                while self.bj._peek() in '0123456789':
+                    num += self.bj._get()
+            # Read possible decimal part and convert to float
+            if self.bj._peek() == '.':
+                self.bj._get()
+                num += '.' + self.bj._get()
+                if num[-1] not in '0123456790':
+                    raise Exception('Expected digit after dot!')
+                while self.bj._peek() in '0123456789':
+                    num += self.bj._get()
+                self.number = float(num)
+            else:
+                self.number = int(num)
+            # Read possible exponent
+            if self.bj._peek() in 'eE':
+                self.bj._get()
+                exp = self.bj._get()
+                exp_neg = False
+                if exp == '-':
+                    exp_neg = True
+                    exp = self.bj._get()
+                elif exp == '+':
+                    exp = self.bj._get()
+                while self.bj._peek() in '0123456789':
+                    exp += self.bj._get()
+                exp = int(exp)
+                exp = 10 ** exp
+                if exp_neg:
+                    self.number = float(self.number) / exp
+                else:
+                    self.number *= exp
+
         # TODO: Check string!
         elif self.bj._skip_if_next('['):
             self.type = Node.TYPE_ARRAY
             self.fully_read = False
             # TODO: Replace this single lookup position with some kind of lookup table!
             self.children_begin = self.bj._tell_read_pos()
+
         elif self.bj._skip_if_next('{'):
             self.type = Node.TYPE_OBJECT
             self.fully_read = False
             raise Exception('Not implemented yet!')
+
         else:
             raise Exception('Unexpected bytes!')
 
@@ -142,7 +200,7 @@ class Node:
             return False
         if self.type == Node.TYPE_TRUE:
             return True
-        if self.type == Node.TYPE_NUM:
+        if self.type == Node.TYPE_NUMBER:
             return self.number
         if self.type == Node.TYPE_STRING:
             return self.string
@@ -151,7 +209,7 @@ class Node:
     def _read_fully(self):
 
         # If already fully read
-        if self.type in [Node.TYPE_NULL, Node.TYPE_FALSE, Node.TYPE_TRUE, Node.TYPE_NUM, Node.TYPE_STRING]:
+        if self.type in [Node.TYPE_NULL, Node.TYPE_FALSE, Node.TYPE_TRUE, Node.TYPE_NUMBER, Node.TYPE_STRING]:
             return
 
         if self.type == Node.TYPE_ARRAY:
