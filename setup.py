c1c1105db7307683cb116b73f49adb7e09a35540
Henrik Heino <henrik.heino@gmail.com>
2016-08-07 21:41:11 +0300

    setup.py added.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+setup(
+    name='bigjson',
+    version='1.0',
+    packages=['bigjson'],
+    description='Python library that reads JSON files of any size.',
+    author='Henrik Heino',
+    author_email='henrik.heino@gmail.com',
+    url='https://github.com/henu/bigjson',
+    license='MIT',
+)
