f590e2df41425bd25fdf0c8251bb935342bc88af
Henrik Heino <henrik.heino@gmail.com>
2020-12-15 01:01:17 +0200

    v1.0.6: Update website

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(
     name='bigjson',
-    version='1.0.5',
+    version='1.0.6',
     packages=['bigjson'],
     description='Python library that reads JSON files of any size.',
     author='Henrik Heino',
     author_email='henrik.heino@gmail.com',
-    url='https://github.com/henu/bigjson',
+    url='https://henu.fi/bigjson',
     license='MIT',
 )
