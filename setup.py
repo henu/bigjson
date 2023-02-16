9bded9e6289a1ec7fcb15c51fa958cf1e510f368
KOLANICH <kolan_n@mail.ru>
2023-02-16 19:29:37 +0300

    Move the metadata into `setup.cfg`, supported since Dec 2016

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python
-import os
 from setuptools import setup
 
-setup(
-    name='bigjson',
-    version='1.0.9',
-    packages=['bigjson'],
-    description='Python library that reads JSON files of any size.',
-    author='Henrik Heino',
-    author_email='henrik.heino@gmail.com',
-    url='https://henu.fi/bigjson',
-    project_urls = {
-        "Source Code": "https://github.com/henu/bigjson",
-        "Issue Tracker": "https://github.com/henu/bigjson/issues",
-    },
-    license='MIT',
-)
+if __name__ == "__main__":
+    setup()
